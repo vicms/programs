eeaa65a36c2000b5194ceb8fe22b8075ad138991
Víctor Martinez <Victor@macintosh.lan>
2018-02-24 14:46:01 -0600

    genesis

---
+++ src/main/java/project2/artifact/Lambdas.java
@@ -0,0 +1,35 @@
+package project2.artifact;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Lambdas {
+	
+	public String hola="sad1";
+	Map<Integer, Integer> algo = new HashMap<Integer,Integer>();
+	
+	List<String> lista = new ArrayList<String>();
+	
+//	 public Lambdas algwo = new Lambdas(){
+//		
+//		 public String hola2="sad2";
+//
+//	
+//	};
+
+	
+	
+	
+	
+//		
+//	 public static void main( String[] args )
+//	    {
+//	     //   System.out.println( "Hello World!");
+//	        //System.out.println(new Lambdas().algwo.hola);
+//	        
+//	    }
+
+}
